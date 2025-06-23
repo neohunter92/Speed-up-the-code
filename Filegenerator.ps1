e64610d59b2f69a0c9f239ce54c491275181f2fd
Laszlo Forgo <laszlo.forgo@braincon.de>
2025-06-23 21:48:10 +0000

    Refactor code formatting for improved readability and consistency

--- Filegenerator.ps1
+++ Filegenerator.ps1
@@ -3,23 +3,23 @@ Measure-Command {
     $plcNames = 'PLC_A','PLC_B','PLC_C','PLC_D'
     $errorTypes = 'Sandextrator overload', 'Conveyor misalignment', 'Valve stuck', 'Temperature warning'
     $statusCodes = 'OK','WARN','ERR'
- 
+
     $writer = [System.IO.StreamWriter]::new($bigFileName, $false)
     $rnd = [System.Random]::new()
- 
+
     for ($i = 0; $i -lt 50000; $i++) {
         $timestamp = [DateTime]::Now.AddSeconds(-$i).ToString('yyyy-MM-dd HH:mm:ss')
         $plc = $plcNames[$rnd.Next(0, $plcNames.Count)]
         $operator = $rnd.Next(101,121)
-        $batch = $rnd.Next( 1000,1101)
+        $batch = $rnd.Next(1000,1101)
         $status = $statusCodes[$rnd.Next(0, $statusCodes.Count)]
         $machineTemp = [math]::Round(($rnd.Next(60,110)) + ($rnd.Next()),2)
-        $load = $rnd.Next(0, 101)
- 
-        if (($rnd.Next( 1, 8)) -eq 4) {
+        $load = $rnd.Next(0,101)
+
+        if (($rnd.Next(1,8)) -eq 4) {
             $errorType = $errorTypes[$rnd.Next(0, $errorTypes.Count)]
             if ($errorType -eq 'Sandextrator overload') {
-                $value = $rnd.Next( 1 ,11)
+                $value = $rnd.Next(1,11)
                 $writer.WriteLine("ERROR; $timestamp; $plc; $errorType; $value; $status; $operator; $batch; $machineTemp; $load")
             } else {
                 $writer.WriteLine("ERROR; $timestamp; $plc; $errorType; ; $status; $operator; $batch; $machineTemp; $load")
@@ -27,10 +27,8 @@ Measure-Command {
         } else {
             $writer.WriteLine("INFO; $timestamp; $plc; System running normally; ; $status; $operator; $batch; $machineTemp; $load")
         }
- 
     }
- 
     $writer.Close()
     $writer.Dispose()
-    $null = [Console]::WriteLine('PLC log file generated.')
+    [Console]::WriteLine('PLC log file generated.')
 }
