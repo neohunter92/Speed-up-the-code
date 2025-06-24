d246e7156fe74dc0990019f1ffb0829881a584ca
Laszlo Forgo <laszlo.forgo@braincon.de>
2025-06-24 17:17:49 +0000

    Refactor array length access for consistency and improved readability

--- Filegenerator.ps1
+++ Filegenerator.ps1
@@ -9,17 +9,17 @@ Measure-Command {
 
     for ($i = 0; $i -lt 50000; $i++) {
         $timestamp = [DateTime]::Now.AddSeconds(-$i).ToString('yyyy-MM-dd HH:mm:ss')
-        $plc = $plcNames[$rnd.Next(0, $plcNames.Count)]
+        $plc = $plcNames[$rnd.Next(0, $plcNames.Length)]
         $operator = $rnd.Next(101,121)
         $batch = $rnd.Next(1000,1101)
-        $status = $statusCodes[$rnd.Next(0, $statusCodes.Count)]
+        $status = $statusCodes[$rnd.Next(0, $statusCodes.Length)]
         $machineTemp = [math]::Round(($rnd.Next(60,110)) + ($rnd.Next()),2)
         $load = $rnd.Next(0,101)
 
         if (($rnd.Next(1,8)) -eq 4) {
-            $errorType = $errorTypes[$rnd.Next(0, $errorTypes.Count)]
+            $errorType = $errorTypes[$rnd.Next(0, $errorTypes.Length)]
             if ($errorType -eq 'Sandextrator overload') {
-                $value = $rnd.Next(1,11)
+                $value = $rnd.Next(1,11);
                 $writer.WriteLine("ERROR; $timestamp; $plc; $errorType; $value; $status; $operator; $batch; $machineTemp; $load")
             } else {
                 $writer.WriteLine("ERROR; $timestamp; $plc; $errorType; ; $status; $operator; $batch; $machineTemp; $load")
@@ -28,7 +28,5 @@ Measure-Command {
             $writer.WriteLine("INFO; $timestamp; $plc; System running normally; ; $status; $operator; $batch; $machineTemp; $load")
         }
     }
-    $writer.Close()
-    $writer.Dispose()
-    [Console]::WriteLine('PLC log file generated.')
+    $writer.Close();$writer.Dispose();[Console]::WriteLine('PLC log file generated.')
 }
